16388e375d0f6264866361ac64729e9c40ce46cf
miltonbjork-cloud <miltonbjork@icloud.com>
2025-08-14 08:47:14 +0200

    Create constants.py

---
+++ utils/constants.py
@@ -0,0 +1,14 @@
+BUILDING_TYPES = {
+    "Alla": "",
+    "Hyresrätt": "1",
+    "Bostadsrätt": "2",
+}
+
+QUEUES = {
+    "Ungdom": "1",
+    "Vuxen": "2",
+}
+
+YEARS = [2025]  # eller fler år som du vill hämta
+
+QUEUE_TIME_GROUPS = ["1", "2", "3"]
